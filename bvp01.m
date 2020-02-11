fc1f49b71d624d3c6e1c35f1daa296942f8789c4
Jan Mandel <jan.mandel@gmail.com>
2020-02-11 08:41:17 -0700

    comments, plot

--- bvp01.m
+++ bvp01.m
@@ -1,6 +1,7 @@
 function [u,x]=bvp(f,n)
 % [u,x]=bvp(f,n)
 % solve -u''=f, u(0)=u(1)=0 on uniform mesh x(0) to x(n+1)=1
+% Jan Mandel, Feb 10 2020 
 h=1/(n+1);
 x=[1:n]'/(n+1); 
 b=h*h*f(x);
--- bvp01_test.m
+++ bvp01_test.m
@@ -1,5 +1,7 @@
-% example 2.5
 function bvp01_test(n)
+% plot solution of -u''=(3*x + x.^2).*exp(x), u(0)=u(1)=0
+% example 2.5 on n points mesh + boundary
+% Jan Mandel, Feb 10 2020 
 if ~exist('n')
     n=5;
 end
@@ -10,7 +12,7 @@ m=1000;
 xx=[0:m+1]'/(m+1);
 ss = sol(xx);
 s = sol(x);
-plot(x,u,'o--',xx,ss,'-');
+plot([0;x;1],[0;u;0],'o--',xx,ss,'-'); % boundary values not part of solution x
 legend('numerical','exact')
 err=max(abs(s-u));
 title([num2str(n),' points error ',num2str(err)])
