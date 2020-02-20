964fc300ade1e6fa9560076f9e07af42013f41eb
Jan Mandel <jan.mandel@gmail.com>
2020-02-19 23:25:41 -0700

    cp fixed solve_gauss from 4650

--- solve_gauss.m
+++ solve_gauss.m
@@ -6,22 +6,28 @@ function v=solve_gauss(A,b)
         error('matrix A must be square')
     end
     pr='Presss ENTER to continue >';
-    clc,A,b,input(pr);
+    % clc,A,b,input(pr);
+    A0=A
+    b0=b
     for i=1:n-1
         clc
-        m        = A(i+1,i)/A(i,i)
-        A(i+1,:) = A(i+1,:)-m*A(i,:)
-        b(i+1)   = b(i+1) - m*b(i)  
+        i 
+        for j=i+1:n
+            m      = A(j,i)/A(i,i)
+            A(j,i:n) = A(j,i:n)-m*A(i,i:n) 
+            b(j)     = b(j) - m*b(i)
+        end
         input(pr);
     end
     % back substitution
     v=zeros(n,1); % preallocate as column
     v(n)=b(n)/A(n,n)
-    input(pr);
+    % input(pr);
     for i=n-1:-1:1
-        clc, A, b
-        v(i) = (b(i) - A(i,i+1)*v(i+1))/A(i,i)
+        clc
+        A, b
+        v(i) = (b(i) - A(i,i+1:end)*v(i+1:end))/A(i,i)
         input(pr);
     end
-    err = norm(A*v - b,1)
+    err = norm(A0*v - b0,1)
 end
