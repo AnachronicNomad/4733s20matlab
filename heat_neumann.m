380cdcfe4aa876decc3044bdf02fd46f1e50e667
Jan Mandel <jan.mandel@gmail.com>
2020-03-30 20:53:37 -0600

    in class

--- heat_neumann.m
+++ heat_neumann.m
@@ -6,24 +6,27 @@ function heat_neumann(f,n,T,N)
 % n  number of cosine terms 
 % T  length of the time interval
 % N  number of points to display in x and in t
-c0=2*integral(f,0,1); 
-for k=1:n
-    c(k)=2*integral(@(x)f(x).*cos(k*pi*x),0,1);
-end
+figure(1)
+c=cos_fourier(f,n);
 [xx,tt]=ndgrid(linspace(0,1,N),linspace(0,T,N));
-sol=c0*ones(size(xx))*0.5;
+sol=c(1)*ones(size(xx))*0.5;
 k=0;
+mins=0;maxs=eps;
+figure(2)
 show
 for k=1:n
-    sol = sol + c(k)*exp(-(k*pi)^2*tt).*cos(k*pi*xx);
+    sol = sol + c(k+1)*exp(-(k*pi)^2*tt).*cos(k*pi*xx);
     show
 end
 mesh(sol)
     function show
-    mesh(xx,tt,sol),
+    mesh(xx,tt,sol)
+    mins=min(min(sol(:)),mins);maxs=max(max(sol(:)),maxs);
+    axis([0,1,0,T,mins,maxs])
     title(['n=',num2str(k)])
     xlabel('x')
     ylabel('time')
+    zlabel('u')
     drawnow
     pause(0.5)
     end
