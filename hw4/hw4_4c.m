95190cd20a1f6a7ef4d73ccc0f003ecd3b46ba69
Jan Mandel <jan.mandel@gmail.com>
2020-04-03 20:18:13 -0600

    hw4

--- hw4/hw4_4c.m
+++ hw4/hw4_4c.m
@@ -1,7 +1,14 @@
+% homework 4 problem 4c
+% solve the pde  u_t=u_xx on (0,1), u(0)=u(1)=0, u(x,0)=sin(pi*x)^3
 pi=4*atan(sym(1))
 syms x t k
 f = sin(pi*x)^3
-c = simplify(int(f*sin(k*pi*x),x,0,1))
-c1 = int(f*sin(pi*x),x,0,1)
-c3 = int(f*sin(3*pi*x),x,0,1)
-u = simplify(c1*exp(-pi^2*x)*sin(pi*x)+c3*exp(-(3*pi)^2*x)*sin(3*pi*x))
+c = simplify(2*int(f*sin(k*pi*x),x,0,1))
+c1 = 2*int(f*sin(pi*x),x,0,1)
+c3 = 2*int(f*sin(3*pi*x),x,0,1)
+u = simplify(c1*exp(-pi^2*t)*sin(pi*x)+c3*exp(-(3*pi)^2*t )*sin(3*pi*x))
+% latex(u)
+u0 = expand(subs(u,t,0))
+u0=sin(pi*x)*simplify(u0/sin(pi*x))
+error_0 = u0 - f
+error_u = diff(u,t)-diff(u,x,2)
