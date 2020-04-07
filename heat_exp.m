81c474354650962d7fb9b3130ba5234c32382e47
Jan Mandel <jan.mandel@gmail.com>
2020-04-07 00:28:54 -0600

    heat_exp.m

--- heat_exp.m
+++ heat_exp.m
@@ -5,12 +5,12 @@ function heat_exp(f,n,T)
 % T max time
 % nt points
 dx=1/(n+1)
-dt=2.1*dx*dx
+dt=0.50*dx*dx
 r=dt/(dx*dx)
 nt=ceil(T/dt);
 v=zeros(1,n+2);
-vnew=v;
-v(2:n+1)=f(dx*[1:n]);
+% vnew=v;
+v=f(dx*[0:n+1]);
 t=0;
 x=dx*[0:n+1];
 one=ones(1,n+2);
@@ -19,6 +19,7 @@ for m=1:nt+1
     plot3(x,t*one,v),xlabel('x'),ylabel('t'),zlabel('v'),grid on,drawnow,hold on
     t = t+dt;
     % need indexing from 1:n+2 instead 0:n+1
+    vnew=zeros(1,n+2);
     for j=2:n+1
         vnew(j) = v(j) + dt * (v(j-1)-2*v(j)+v(j+1))/(dx*dx);
     end
