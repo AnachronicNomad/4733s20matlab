975a1c31e85f2b2bb6de3833d504f51bdccf9e13
Jan Mandel <jan.mandel@gmail.com>
2020-04-07 19:18:45 -0600

    stmt

---
+++ hw4/fourier_x.m
@@ -0,0 +1,5 @@
+% Write a computer program that computes the Nth partial sums of Fourier series. 
+% Use the program to plot the function f(x) = x and the corresponding Fourier 
+% sine and Fourier cosine series on the unit interval. Then use the program to 
+% plot the series for x ∈ [−3, 3].
+
