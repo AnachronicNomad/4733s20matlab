8c954f79d96ad731cb6a4e4e17614b8229b48edb
Jan Mandel <jan.mandel@gmail.com>
2020-04-08 16:38:50 -0600

    done

--- heat_imp.m
+++ heat_imp.m
@@ -7,7 +7,7 @@ function heat_imp(f,n,T)
 % example: heat_exp(@(x)cos(2*pi*x),50,0.1)
 if ~nargin, help(mfilename), return, end
 dx=1/(n+1)
-dt=0.5*dx*dx
+dt=dx
 r=dt/(dx*dx)
 nt=ceil(T/dt);
 v=zeros(n+2,1);
@@ -19,6 +19,7 @@ one=ones(1,n+2)';
 figure(gcf),clf, hold off   % clear and show figure window
 % matrix A needs to produce u=A*v with  u(j)=v(j-1)-2*v(j)+v(j+1)
 A=spdiags(ones(n,1)*[-1,2,-1],[-1,0,1],n,n);
+AA=speye(n)+(dt/(dx*dx))*A;
 for m=1:nt+1
     plot3(x,t*one,v),xlabel('x'),ylabel('t'),zlabel('v'),grid on,drawnow,hold on
     t = t+dt;
@@ -29,7 +30,10 @@ for m=1:nt+1
     % end
     % solve for new v instead
     % start by writing the explicit code as
-    vnew(2:n+1) = v(2:n+1) - (dt/(dx*dx))*A*v(2:n+1);
+    % vnew(2:n+1) = v(2:n+1) - (dt/(dx*dx))*A*v(2:n+1);
+    % vnew(2:n+1)+(dt/(dx*dx))*A*vnew(2:n+1) = v(2:n+1);
+    % (I+(dt/(dx*dx))*A)*vnew(2:n+1) = v(2:n+1)
+    vnew(2:n+1) = AA\v(2:n+1);
     v=vnew;
 end
 hold off
